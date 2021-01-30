6647b9e44244f4030a6681182cdc1c414a4707ca
Cloud User <ec2-user@ip-10-0-0-8.eu-west-1.compute.internal>
2021-01-30 04:25:02 +0000

    final update

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+
+echo "Cloning repository from git to deploy new changes"
+sleep 5
+rm -rf basic-flask-app
+git clone https://github.com/abhi2alone/basic-flask-app.git
+echo "Git repository cloned to local machine"
+sleep 10
+cd basic-flask-app
+docker kill talentica-app
+docker rm talentica-app
+docker build .
+docker build --tag talentica-app .
+docker run -d --name talentica-app  -p 5000:5000 talentica-app
+exit 0
